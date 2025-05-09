bf3400d1e7930008ffe804e6196c8857b8fb9480
louis <louis@debian>
2025-05-10 00:52:36 +0200

    correct launch_conky.sh

---
+++ launch_conky.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+conky -c ~/.conky/net_down_panel &
+conky -c ~/.conky/net_up_panel &
+conky -c ~/.conky/time_panel &
+conky -c ~/.conky/date_panel &
+conky -c ~/.conky/cpu_panel &
+conky -c ~/.conky/mem_panel &
+conky -c ~/.conky/disk_panel &
--- lunch_conky.sh
+++
@@ -1,7 +0,0 @@
-conky -c net_down_panel &
-conky -c net_up_panel &
-conky -c time_panel &
-conky -c date_panel &
-conky -c cpu_panel &
-conky -c mem_panel &
-conky -c disk_panel &
