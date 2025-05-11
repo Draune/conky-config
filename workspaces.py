3d620d9bb41c73a25a724321ef41568d2b4ab55e
louis <louis@debian>
2025-05-11 16:52:54 +0200

    renamed variables

--- workspaces.py
+++ workspaces.py
@@ -2,20 +2,20 @@ import i3ipc
 # Connexion à i3
 i3 = i3ipc.Connection()
 
-current_ws = open("/tmp/conky_current_ws", "w")
+current_ws_file = open("/tmp/conky_current_ws", "w")
 
-ws_file = "/tmp/conky_ws"
-ws = []
+ws_files_prefix = "/tmp/conky_ws"
+ws_files = []
 
 
 for i in range(1, 10):
-    ws.append(open(ws_file+str(i), "w"))
-    ws[i-1].write(f"{i}:")
+    ws_files.append(open(ws_files_prefix+str(i), "w"))
+    ws_files[i-1].write(f"{i}:")
 
 workspaces = i3.get_workspaces()
-for ws_ in workspaces:
-    if ws_.focused:
-        current_ws.write(f"{ws_.num}")
+for ws in workspaces:
+    if ws.focused:
+        current_ws_file.write(f"{ws.num}")
 
 tree = i3.get_tree()
 char = ""
@@ -32,8 +32,8 @@ for window in tree.leaves():
         char = " "
     window_workspace = window.workspace().num
     if window_workspace  < 10:
-        ws[window_workspace-1].write(char)
+        ws_files[window_workspace-1].write(char)
 
-current_ws.close()
-for f in ws:
+current_ws_file.close()
+for f in ws_files:
     f.close()
