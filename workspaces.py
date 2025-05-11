d55e19b9ab40a60180db584c8377b828994324ac
louis <louis@debian>
2025-05-11 21:00:35 +0200

    bug corrections

--- workspaces.py
+++ workspaces.py
@@ -9,6 +9,7 @@ for line in output:
     line = line.split()
     if line[1] == "*":
         current_ws_file.write(str(int(line[0])+1))
+        break
 
 current_ws_file.close()
 
@@ -19,7 +20,7 @@ ws_files = []
 # if you want to change the number of workspaces, change this variable
 nb_workspaces=9
 
-for i in range(1, nb_worspaces+1):
+for i in range(1, nb_workspaces+1):
     ws_files.append(open(ws_files_prefix+str(i), "w"))
     ws_files[i-1].write(f"{i}:")
 
@@ -29,8 +30,10 @@ icons_list = [["discord", ""],
               ["", ""]] # default
 
 output = subprocess.check_output(["wmctrl", "-l"]).decode()
-output = split("\n")
+output = output.split("\n")
 for line in output:
+    if line.strip() == "":
+        break
     window_name = line.lower()
     line = line.split()
     num_workspace = int(line[1])
@@ -39,7 +42,9 @@ for line in output:
 
     for icon in icons_list:
         if icon[0] in window_name:
-            ws_files[window_workspace].write(" "+icon[1])
+            ws_files[num_workspace].write(" "+icon[1])
+            break
+
 
 for f in ws_files:
     f.close()
