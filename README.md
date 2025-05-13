1f0b3404d9ca9d48d123fb4bdeb5201d6e63b77b
louis <louis@debian>
2025-05-13 21:18:31 +0200

    changes to have the possibility to use i3ipc

--- README.md
+++ README.md
@@ -6,13 +6,25 @@
 
 ![conky_image](https://github.com/Draune/conky-config/blob/master/screenshots/conky.png)
 
+## Scripts for workspaces
+
+To use the script for workspace for another X server wm you need to change `workspaces_i3.py` to `workspaces.py` in `ws1_panel`.
+
+If you want to use more than 6 workspaces, change the script's `nb_workspaces` variable.
+
+For i3 users who are not on debian, you'll probably want to change the `name_conky_window` variable so it will not display the conkys window in the workspaces.
+
 ## Requirements
 
 You will need 'Cousine Nerd Font' and 'Symbols Nerd font Mono' installed in your /usr/share/fonts (just unzip them there and do `fc-cache -fv`).
 
-You will also need wmctrl:
+You will also need i3ipc or  wmctrl:
 
 ``` shell
+# for i3 wm
+pip install i3ipc
+
+# for non other wm
 sudo apt install wmctrl
 ```
 
---
+++ workspaces_i3.py
@@ -0,0 +1,49 @@
+import i3ipc
+i3 = i3ipc.Connection()
+
+# Current workspace
+current_ws_file = open("/tmp/conky_current_ws", "w")
+
+workspaces = i3.get_workspaces()
+for ws in workspaces:
+    if ws.focused:
+        current_ws_file.write(f"{ws.num}")
+
+current_ws_file.close()
+
+# Windows in each workspace
+ws_files_prefix = "/tmp/conky_ws"
+ws_files = []
+
+# if you want to change the number of workspaces, change this variable
+nb_workspaces=6
+
+for i in range(1, nb_workspaces+1):
+    ws_files.append(open(ws_files_prefix+str(i), "w"))
+    # ws_files[i-1].write(f"{i}:")
+
+icons_list = [["discord", ""],
+              ["emacs", ""],
+              ["firefox",""],
+              ["xonsh", ""],
+              ["gdb", ""],
+              ["setting", ""],
+              ["signal", "󰻞"],
+              ["", ""]] # default
+
+name_conky_window = "conky (debian)"
+
+tree = i3.get_tree()
+for window in tree.leaves():
+    window_name = window.name.lower()
+    num_workspace = window.workspace().num
+    if (num_workspace > nb_workspaces or num_workspace < 0 or name_conky_window in window_name):
+        continue
+
+    for icon in icons_list:
+        if icon[0] in window_name:
+            ws_files[num_workspace-1].write(""+icon[1])
+            break
+
+for f in ws_files:
+    f.close()
